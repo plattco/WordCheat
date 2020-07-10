3edae8a4bda7d16c71d7cd3bf255893b35c00ebb
plattcr <plattcr@miamioh.edu>
2020-07-10 11:42:06 -0400

    adding wordCheat 1.0 to github

---
+++ WordCheatFinal/WordCheatFinal/EnglishWords.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Reflection;
+
+namespace WordCheatFinal
+{
+	public class EnglishWords
+	{
+		public string[] lines;
+		public EnglishWords(string fileName)
+		{
+			var assembly = IntrospectionExtensions.GetTypeInfo(typeof(EnglishWords)).Assembly;
+			string namespaceName = "WordCheatFinal";
+			Stream stream = assembly.GetManifestResourceStream(namespaceName + "." + fileName);
+			string text = "";
+			using(var dictReader = new System.IO.StreamReader(stream))
+            {
+				text = dictReader.ReadToEnd();
+            }
+			lines = text.Split('\r','\n');
+		}
+
+		public bool isLegal(string word)
+		{
+			if (Array.Exists(lines, element => element == word))
+			{
+				return true;
+			}
+			return false;
+		}
+
+		//find all possible permutations of string letters
+		//not sure how to find permutations of a certain length
+		//put these permutations in an array
+		//use for loop to iterate over every word in dictionary
+		//use nested loop to iterate over every permuation in the permutation array
+		//for each nested iteration compare the permutation to the word in the array
+		//if it is equal add the word to the result array
+
+		//or
+
+		//create array to hold results
+		//loop through every word in lines array
+		//check if the string letters contains every letter in the word
+		//if it is true then add it to results array
+		public List<string> AvailableWords(string letters, int len, bool allowDup)
+		{
+			List<string> results = new List<string>();
+			for (int i = 0; i < lines.Length; i++)
+			{
+				string candidate = lines[i];
+				if (candidate.Length == len)
+				{
+					bool accepted = true;
+					int found;
+					string letterTemp = letters;
+					foreach (char c in candidate)
+					{
+						found = letterTemp.IndexOf(c);
+						if (found != -1 && allowDup == false)
+						{
+							letterTemp = letterTemp.Remove(found, 1);
+						}
+						else if (found == -1)
+						{
+							accepted = false;
+							break;
+						}
+					}
+					if (accepted)
+					{
+						results.Add(candidate);
+					}
+				}
+			}
+			return results;
+
+			/*
+			 * string[] results;
+			for (int i = 0; i > lines.Length; i++)
+				string candidate = lines[i];
+				if(candidate.Length == len)
+				{
+					foreach(char c in candidate)
+					{
+						if (!(letters.Contains((string)c)))
+						{
+							// results = results + lines[i];
+						}
+						else
+						{
+						results = results + candidate;
+						}
+					}
+				}*/
+			/*
+			  for(int i = 0; i > lines.Length; i++)
+			{
+				if (lines[i].Length == len)
+				{
+					for (int j = 0; j > len; j++)
+					{
+						if (lines[j] == letters[i])
+						{
+							results = results + lines[j];
+						}
+					}
+				}
+			}*/
+		}
+
+
+		public List<string> NYTSpellingBee(string outerRingOfLetters, char centerLetter)
+		{
+			return null;
+		}
+	}
+}
---
+++ WordCheatFinal/WordCheatFinal/MainPage.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Xamarin.Forms;
+
+
+namespace WordCheatFinal
+{
+    class MainPage : ContentPage
+    {
+        public EnglishWords EnglishDictionary = new EnglishWords("words.txt");
+        Entry Letters;
+        Entry Count;
+        Button Clear;
+        Button Find;
+        CheckBox Dup;
+        Label DupLabel;
+        Label Display;
+
+        public MainPage()
+        {
+            Letters = new Entry { Text = "Enter letters" };
+            Clear = new Button();
+            Clear.HeightRequest = 75;
+            Clear.WidthRequest = 190;
+            Clear.Text = "CLEAR";
+            Clear.FontSize = 40;
+            Clear.TextColor = Color.Black;
+            Find = new Button();
+            Find.HeightRequest = 75;
+            Find.WidthRequest = 190;
+            Find.Text = "FIND";
+            Find.FontSize = 40;
+            Find.TextColor = Color.Black;
+            Dup = new CheckBox();
+            DupLabel = new Label();
+            DupLabel.Text = "Duplicate Letters?";
+            DupLabel.FontSize = 18;
+            //         Dup.Text = "Duplicate Letters?"
+            Count = new Entry { Text = "Length" };
+            Display = new Label();
+            Display.Text = "Results";
+            Display.HorizontalTextAlignment = Xamarin.Forms.TextAlignment.Center;
+
+            StackLayout panel = new StackLayout();
+            StackLayout panel2 = new StackLayout();
+            StackLayout innerPanel = new StackLayout();
+            ScrollView scrolly = new ScrollView();
+
+            panel.Children.Add(Letters);
+            //panel.Children.Add(Clear);
+            //panel.Children.Add(Find);
+            panel.Children.Add(panel2);
+            panel.Children.Add(innerPanel);
+            // panel.Children.Add(Dup);
+            panel.Children.Add(Count);
+            scrolly.Content = Display;
+            panel.Children.Add(scrolly);
+            //panel.Children.Add(Display);
+            panel.Spacing = 10;
+            panel.Padding = new Thickness(10, 20, 10, 10);
+            panel2.Children.Add(Clear);
+            panel2.Children.Add(Find);
+            panel2.Spacing = 10;
+            panel2.Orientation = StackOrientation.Horizontal;
+            innerPanel.Orientation = StackOrientation.Horizontal;
+            innerPanel.Children.Add(DupLabel);
+            innerPanel.Children.Add(Dup);
+            
+
+          /*  StackLayout panel2 = new StackLayout();
+            panel2.HorizontalOptions = LayoutOptions.Center;
+            panel2.Orientation = StackOrientation.Horizontal;
+            panel2.Children.Add(Clear);
+            panel2.Children.Add(Find);
+            panel2.Spacing = 20;
+            panel2.Padding = new Thickness(10, 10, 10, 10);
+
+            */
+
+            this.Content = panel;
+            //this.Content = panel2;
+            
+
+            Find.Clicked += OnClick;
+            Clear.Clicked += Clear_Clicked;
+        }
+
+        private void Clear_Clicked(object sender, EventArgs e)
+        {
+            Display.Text = "Results";
+        }
+
+        private void OnClick(object sender, EventArgs e)
+        {
+            int temp = int.Parse(Count.Text);
+            
+            List<string> possibleWords = EnglishDictionary.AvailableWords(Letters.Text.ToUpper(), temp, Dup.IsChecked);
+            string resulty = String.Join("\r\n", possibleWords);//possibleWords.ToString();
+            Display.Text = resulty;
+            }
+
+       
+        }
+    }
